8e47efc2a0c3d68aec2ebab64c02ad2832079408
abhi-zero <abhi.0zero@gmail.com>
2025-08-23 19:12:23 +0530

    Add Router and create paths for navigation

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,44 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { RouterProvider, createBrowserRouter } from "react-router";
 
-createRoot(document.getElementById('root')).render(
+import {Home, NewArticles, Popular, Trending, Categories} from "./pages/index.js";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+       {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/new",
+        element: <NewArticles />,
+      },
+      {
+        path: "/popular",
+        element : <Popular />
+      },
+      {
+        path: "/trending",
+        element : <Trending/>
+      },
+      {
+        path: "/categories",
+        element : <Categories />
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router}>
+      <App />
+    </RouterProvider>
+  </StrictMode>
+);
