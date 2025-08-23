a9e5d512900c928cef58df968f5c526da48784b4
abhi-zero <abhi.0zero@gmail.com>
2025-08-23 21:57:29 +0530

    Read more btn

---
+++ src/components/ReadMoreBtn.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function() {
+  return (
+    <button className='bg-[#F15E50] w-fit  py-3 px-8 font-bold tracking-wide text-[#00001A] hover:bg-[#fc7367] hover:shadow-2xl hover:shadow-[#fc7367] cursor-pointer transition-colors duration-300 ease-in-out'>
+        Read More
+    </button>
+  )
+}
