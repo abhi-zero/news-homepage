1312cd7c87e432578b43848ef4727b1013968fb9
abhi-zero <abhi.0zero@gmail.com>
2025-08-23 22:12:20 +0530

    ADD componnet in Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
-import { HeroNews } from '../components'
+import { HeroNews, NewArticles} from '../components'
 
 const Home = () => {
   return (
-    <div className='max-w-[1440px] m-auto'>
-     <HeroNews />
-    
+    <div className='max-w-[1440px] m-auto px-[30px] md:grid md:grid-cols-2  lg:grid-cols-3 gap-5'>
+     <div className='md:col-start-1  lg:col-start-1 lg:col-end-3'>
+      <HeroNews />
+     </div>
+     <div className='md:col-start-2 lg:col-start-3'>
+        <NewArticles />
+     </div>
+     
     </div>
   )
 }
