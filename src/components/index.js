88fe2eec778e966c3384dd82803e1a50a45fee97
abhi-zero <abhi.0zero@gmail.com>
2025-08-23 21:58:01 +0530

    All the Export form one location

---
+++ src/components/index.js
@@ -0,0 +1,11 @@
+import Header from "./Header/Header";
+import HeroNews from "./HeroNews/HeroNews";
+import ReadMoreBtn from "./ReadMoreBtn";
+import NewArticles from "./NewArticles/NewArticles";
+
+export {
+    Header,
+    HeroNews,
+    ReadMoreBtn,
+    NewArticles
+}
