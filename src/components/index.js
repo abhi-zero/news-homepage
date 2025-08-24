8655f5a70f7946f97188f3d55270d1b8fb867fc3
abhi-zero <abhi.0zero@gmail.com>
2025-08-24 09:50:38 +0530

    Add card to the index js

--- src/components/index.js
+++ src/components/index.js
@@ -2,10 +2,12 @@ import Header from "./Header/Header";
 import HeroNews from "./HeroNews/HeroNews";
 import ReadMoreBtn from "./ReadMoreBtn";
 import NewArticles from "./NewArticles/NewArticles";
+import Card from "./Card/Card";
 
 export {
     Header,
     HeroNews,
     ReadMoreBtn,
-    NewArticles
+    NewArticles,
+    Card
 }
